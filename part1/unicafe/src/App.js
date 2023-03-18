3b1aa1d5eaad34b03a4758addc1af7a4b71fac0a
Praewpiraya Wiwatphonthana <praewpiraya.wiwatphonthana@aalto.fi>
2023-03-19 00:33:18 +0200

    update

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 
+
+
+
 const App = () => {
   // save clicks of each button to its own state
+  const firstheaderName = 'give feedback'
+  const secondheaderName = 'statistics'
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
+      <h1>{firstheaderName}</h1>
       code here
+      <h1>{secondheaderName}</h1>
     </div>
   )
 }
