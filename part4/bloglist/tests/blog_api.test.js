78160ef61e121fa703ba86f588d04d090425fbd9
Praewpiraya Wiwatphonthana <praewpiraya.wiwatphonthana@aalto.fi>
2023-07-09 21:11:07 +0300

    4.13-4.14: Blog list expansions, test pass

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -9,12 +9,9 @@ const api = supertest(app)
 // initialize the database before every test with the beforeEach function:
 beforeEach(async () => {
     await Blog.deleteMany({})
-    for (const blog of helper.initialBlogs) {
-      let blogObject = new Blog(blog)
-      await blogObject.save()
-    }
+    await Blog.insertMany(helper.initialBlogs)
 }, 100000)
-
+describe('when there is initially some blogs saved', () => {
 test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -28,6 +25,16 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 }, 100000)    
 
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map(ret => ret.title)
+  expect(contents).toContain(
+    'Canonical string reduction'
+  )
+}, 100000)
+
+})
 
 test('the unique identifier property of the blog posts is named id', async () => {
   const response = await api.get('/api/blogs')
@@ -37,14 +44,7 @@ test('the unique identifier property of the blog posts is named id', async () =>
   }
 }, 100000)
 
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
 
-  const contents = response.body.map(ret => ret.title)
-  expect(contents).toContain(
-    'Canonical string reduction'
-  )
-}, 100000)
 
 //verifies that making an HTTP POST request to the /api/blogs URL successfully creates a new blog post
 test('a valid blog can be added', async () => {
@@ -124,8 +124,69 @@ test('posting a blog without url returns 400 Bad Request', async () => {
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
   }, 100000)
 
+//view and delete test
+describe('viewing a specific blog', () => {
+test('succeeds with a valid id', async () => {
+  const blogsAtStart = await helper.blogsInDb()
 
-  
+  const blogToView = blogsAtStart[0]
+
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const processedBlogToView = JSON.parse(JSON.stringify(blogToView))
+
+  expect(resultBlog.body).toEqual(processedBlogToView)
+}, 100000)
+
+})
+
+describe('deletion of a blog', () => {
+test('succeeds with status code 204 if id is valid', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(
+    helper.initialBlogs.length - 1
+  )
+  const contents = blogsAtEnd.map(r => r.title)
+
+  expect(contents).not.toContain(blogsAtEnd.title)
+}, 100000)
+})
+
+//update blog, like 
+describe('updating of a blog', () => {
+test('a blog can be successfully updated', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToUpdate = blogsAtStart[0]
+
+  console.log(`Updating "${blogToUpdate.title}"`)
+
+  await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send({
+      ...blogToUpdate,
+      likes: blogToUpdate.likes + 1,
+    })
+    .expect(200)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const blog = blogsAtEnd.find(blog => blog.id === blogToUpdate.id)
+
+  expect(blog.likes).toEqual(blogToUpdate.likes + 1)
+}, 100000)
+})
+
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
