40101e027be5a5496169f33dd73b363eddef9e4b
Praewpiraya Wiwatphonthana <praewpiraya.wiwatphonthana@aalto.fi>
2023-07-09 14:52:24 +0300

    4.4: helper functions and unit tests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,19 @@
+
 const dummy = (blogs) => {
     return 1
   }
   
+const totalLikes = (blogs) => {
+    return blogs
+      .map(blog => blog.likes)
+      .reduce((sum, item) => {
+      return sum + item
+      }, 0)
+}
+
+
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
