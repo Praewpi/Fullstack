2bd4487580fd785f34b676290f2059da58665d69
Praewpiraya Wiwatphonthana <praewpiraya.wiwatphonthana@aalto.fi>
2023-07-14 23:45:52 +0300

    add visibility management of the login form

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -14,6 +15,7 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [loginVisible, setLoginVisible] = useState(false)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -22,14 +24,17 @@ const App = () => {
   }, [])
 
   useEffect(() => {
+    // Check if there is a logged-in user in local storage
     const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      // Set the token for authenticated requests
       blogService.setToken(user.token)
     }
   }, [])
-
+   
+  // Make a login request with the provided username and password
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
@@ -38,12 +43,14 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
-
+      // Save the logged-in user to the local storage
       window.localStorage.setItem(
         'loggedBlogAppUser', JSON.stringify(user)
       )
       blogService.setToken(user.token)
+      // Set the user state
       setUser(user)
+      // Clear the username and password fields
       setUsername('')
       setPassword('')
     } catch (exception) {
@@ -59,10 +66,12 @@ const App = () => {
     event.preventDefault()
     console.log(`logging out for ${user.name}`)
 
+    // Clear the logged-in user from the local storage
     window.localStorage.removeItem('loggedBlogAppUser')
+    // Reset the user state
     setUser(null)
   }
-
+  // add block
   const addBlog = (event) => {
     event.preventDefault()
 
@@ -89,30 +98,29 @@ const App = () => {
       })
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        Username:
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        Password:
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  )
+  const loginForm = () => {
+    //the value of the display property is none if we do not want the component to be displayed
+    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+    const showWhenVisible = { display: loginVisible ? '' : 'none' }
 
+    return (
+      <div>
+        <div style={hideWhenVisible}>
+          <button onClick={() => setLoginVisible(true)}>log in</button>
+        </div>
+        <div style={showWhenVisible}>
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+          />
+          <button onClick={() => setLoginVisible(false)}>cancel</button>
+        </div>
+      </div>
+    )
+  }
   const blogForm = () => (
     <form onSubmit={addBlog}>
       <div>
