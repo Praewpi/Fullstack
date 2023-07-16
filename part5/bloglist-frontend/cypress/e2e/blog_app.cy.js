9dc86fbf87fc6bd369233cd93f0e8dea4acbe71a
Praewpiraya Wiwatphonthana <praewpiraya.wiwatphonthana@aalto.fi>
2023-07-16 17:17:02 +0300

    5.18: bloglist end to end testing

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,4 +1,3 @@
-
 //Cypress runs the tests in order
 describe('Blog app', function() {
   beforeEach(function () {
@@ -26,50 +25,30 @@ describe('Blog app', function() {
     cy.get('#username').should('be.visible')
     cy.get('#password').should('be.visible')
   })
-//   it.only('login fails with wrong password', function() {
-//     cy.contains('Login').click()
-//     cy.get('#username').type('mluukkai')
-//     cy.get('#password').type('wrong')
-//     cy.get('#login-button').click()
-    
-//     cy.get('.error')
-//     .should('contain', 'Wrong username or password')
-//     .and('have.css', 'color', 'rgb(255, 0, 0)')
-//     .and('have.css', 'border-style', 'solid')
+   describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('Login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
 
-//     cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+      cy.contains('Matti Luukkainen logged in')
+    })
 
-//   })
+    it('fails with wrong credentials', function() {
+      cy.contains('Login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
 
-//   it('user can login', function () {
-//     cy.contains('Login').click()
-//     cy.get('#username').type('root')
-//     cy.get('#password').type('sekretpw')
-//     cy.get('#login-button').click()
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
 
-    
-//     //expected result,  ensures that the login was successful.
-//     cy.contains('Matti Luukkainen logged in')
-//   })  
-// })
+      cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
 
+    })
+  })
 
-// describe('when logged in', function() {
-
-//   beforeEach(function() {
-//     cy.visit('http://localhost:3000')
-//     cy.contains('Blogs')
-//     cy.contains('Login').click()
-//     cy.get('#username').type('root')
-//     cy.get('#password').type('sekretpw')
-//     cy.get('#login-button').click()
-//   })
-//   it('a new note can be created', function() {
-//     cy.contains('New Blog').click()
-//     cy.get('#title').type('a blog created by cypress')
-//     cy.get('#author').type('Matti Luukkainen root')
-//     cy.get('#url').type('www.test.com')
-//     cy.contains('Create').click()
-//     cy.contains('a blog created by cypress')
-//   })
  })
