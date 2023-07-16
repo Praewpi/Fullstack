14dc1425e9ad9621a52a60cdcf0100b5b5f50048
Praewpiraya Wiwatphonthana <praewpiraya.wiwatphonthana@aalto.fi>
2023-07-16 10:18:06 +0300

    5.14-5.15: Blog list tests

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom/extend-expect'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 //5.13
-test('renders content', () => {
+test('blog renders the blog\'s title and author, but does not render its URL or number of likes by default.', () => {
     const blog = {
     title: 'test blog',
     author: 'test author',
@@ -26,3 +26,52 @@ test('renders content', () => {
     const likesElement = container.querySelector('.likes')
     expect(likesElement).not.toBeVisible()
 })
+
+//5.14
+test('url and number of likes are shown when the button is clicked', async ()  => {
+  const blog = {
+    title: 'test blog',
+    author: 'test author',
+    url: 'test url',
+    likes: 55
+    }
+
+    const { container } = render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('View')
+    await user.click(button)
+    
+    // visible after clicked
+    const urlElement = container.querySelector('.url')
+    expect(urlElement).toBeVisible()
+  
+    const likesElement = container.querySelector('.likes')
+    expect(likesElement).toBeVisible()
+
+})
+
+
+
+// exercise 5.15
+test('if the like button is clicked twice, calls event handler twice', async () => {
+    const blog = {
+      title: 'test blog',
+      author: 'test author',
+      url: 'test url',
+      likes: 55
+    }
+  
+    const mockHandler = jest.fn()
+  
+    render(
+      <Blog blog={blog} updateBlog={mockHandler} />
+    )
+  
+    const user = userEvent.setup()
+    const button = screen.getByText('Like')
+    await user.click(button)
+    await user.click(button)
+  
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
