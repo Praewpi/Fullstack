37373c115891b7916b294a9b650bb6e78c52dfde
Praewpiraya Wiwatphonthana <praewpiraya.wiwatphonthana@aalto.fi>
2023-07-16 14:50:58 +0300

    part5d, test create blog cypass

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -16,11 +16,32 @@ describe('Blog app', function() {
 
   it('user can login', function () {
     cy.contains('Login').click()
-    cy.get('#username').type('typeusername')
-    cy.get('#password').type('typepw')
+    cy.get('#username').type('root')
+    cy.get('#password').type('sekretpw')
     cy.get('#login-button').click()
-
+    
     //expected result,  ensures that the login was successful.
     cy.contains('Matti Luukkainen logged in')
   })  
+})
+
+
+describe('Blog app create blog', function() {
+
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Blogs')
+    cy.contains('Login').click()
+    cy.get('#username').type('root')
+    cy.get('#password').type('sekretpw')
+    cy.get('#login-button').click()
+  })
+  it('a new note can be created', function() {
+    cy.contains('New Blog').click()
+    cy.get('#title').type('a blog created by cypress')
+    cy.get('#author').type('Matti Luukkainen root')
+    cy.get('#url').type('www.test.com')
+    cy.contains('Create').click()
+    cy.contains('a blog created by cypress')
+  })
 })
